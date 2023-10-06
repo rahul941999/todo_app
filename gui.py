2b5ab8e2ca7a48a74fe92c95da50e7f7fc11cb59
rahul <rahul941999@gamil.com>
2023-10-07 00:59:07 +0530

    add todo function to gui

--- gui.py
+++ gui.py
@@ -3,8 +3,17 @@ import PySimpleGUI as gui
 
 
 label = gui.Text("Type a ToDo : ")
-input_txt = gui.InputText(tooltip='Enter ToDo')
+input_txt = gui.InputText(tooltip='Enter ToDo', key='todo')
 add_button = gui.Button('Add')
-window = gui.Window(" ToDo App", layout=[[label],[input_txt,add_button]])
-window.read()
+window = gui.Window(" ToDo App",
+                    layout=[[label], [input_txt, add_button]])
+while True:
+    event, value = window.read()
+    match event:
+        case "Add":
+            todos = functions.read()
+            todos.append(value['todo'] + '\n')
+            functions.write(todos)
+        case gui.WIN_CLOSED:
+            break
 window.close()
